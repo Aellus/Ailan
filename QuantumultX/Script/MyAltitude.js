c1a178b2783740a11331e11f93e0b519d4530c25
Aellus <61090083+Aellus@users.noreply.github.com>
2022-05-15 18:34:32 +0800

    Update MyAltitude.js

--- QuantumultX/Script/MyAltitude.js
+++ QuantumultX/Script/MyAltitude.js
@@ -1,12 +1,11 @@
 /*
-^https:\/\/api\.textnow\.me\/api2.0\/users\/.* url script-response-body https://raw.githubusercontent.com/Aellus/Ailen/master/QuantumultX/Script/MyAltitude.js
 
 ^https:?/\/.*?dayananetworks.myaltitude*.* url script-response-body https://raw.githubusercontent.com/Aellus/Ailen/master/QuantumultX/Script/MyAltitude.js
 
 */
 var obj = JSON.parse($response.body); 
 obj['collect_signals'] = true;
-//obj['keep_for_rendering'] = true;
+obj['keep_for_rendering'] = true;
 $done({body: JSON.stringify(obj)});
 
 
