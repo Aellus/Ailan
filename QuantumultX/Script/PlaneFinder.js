475028608b3ee1cdf6e4b5c1ae7f6369618c4ec8
Aellus <61090083+Aellus@users.noreply.github.com>
2022-08-08 11:22:27 +0800

    Update PlaneFinder.js

--- QuantumultX/Script/PlaneFinder.js
+++ QuantumultX/Script/PlaneFinder.js
@@ -8,6 +8,21 @@
 下载地址：https://apps.apple.com/app/id361273585
 
 使用声明：⚠️⚠️⚠️此脚本仅供学习与交流,禁止转载与贩卖,需在24小时内从设备中删除！⚠️⚠️⚠️
+#!name=PlaneFinder
+#!desc=解锁PlaneFinder高级订阅
+#!openUrl=https://t.me/Functional_Store_Hub
+#!author=@R·E https://t.me/Functional_Store_Hub
+#!homepage=https://github.com/I-am-R-E/Functional-Store-Hub/tree/Master/PlaneFinder
+#!manual=https://t.me/Functional_Store_Hub/10
+#!icon=https://raw.githubusercontent.com/I-am-R-E/Functional-Store-Hub/Master/Files/icon.png
+#!other=PlaneFinder.QuantumultX.snippet
+
+[rewrite_local]
+^https?:\/\/app\-live\.planefinder\.net\/api\/v\d+\/account\/\w+\/login url script-response-body https://raw.githubusercontent.com/I-am-R-E/Functional-Store-Hub/Master/PlaneFinder/Script/PlaneFinder.js
+
+[mitm]
+hostname = app-live.planefinder.net
+
 */
 
 var _0x5ec833=JSON['parse']($response['body']);var _0x430258={};_0x430258['type']='itunes';_0x430258['manageURL']='itms-apps://apps.apple.com/account/subscriptions';_0x430258['expiryTS']=0x70db6800;_0x430258['renews']=!![];_0x5ec833['payload']['subscription']=_0x430258;_0x5ec833['payload']['permissions']=[0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xc,0xd,0xf,0x10];var _0x4cb17f={};_0x4cb17f['body']=JSON['stringify'](_0x5ec833);$done(_0x4cb17f);
