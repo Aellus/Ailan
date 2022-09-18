558eb51e9c5539fa77bc0bd179ee2760caa3b6f0
Aellus <61090083+Aellus@users.noreply.github.com>
2022-09-19 00:21:48 +0800

    Update Xmind.js

--- QuantumultX/Script/Xmind.js
+++ QuantumultX/Script/Xmind.js
@@ -10,4 +10,19 @@
 使用声明：⚠️⚠️⚠️此脚本仅供学习与交流,禁止转载与贩卖,需在24小时内从设备中删除！⚠️⚠️⚠️
 */
 
+#!name=Xmind思维导图
+#!desc=解锁Xmind思维导图订阅到2030年
+#!openUrl=https://t.me/Functional_Store_Hub
+#!author=@R·E https://t.me/Functional_Store_Hub
+#!homepage=https://github.com/I-am-R-E/Functional-Store-Hub/tree/Master/Xmind
+#!manual=https://t.me/Functional_Store_Hub/10
+#!icon=https://raw.githubusercontent.com/I-am-R-E/Functional-Store-Hub/Master/Files/icon.png
+#!other=Xmind.QuantumultX.snippet
+
+#[rewrite_local]
+#https?:\/\/www\.xmind\.(cn|net|app)\/\_res\/devices url script-response-body https://raw.githubusercontent.com/I-am-R-E/Functional-Store-Hub/Master/Xmind/Script/Xmind.js
+
+#[mitm]
+#hostname = www.xmind.cn, www.xmind.net, www.xmind.app
+
 var _0x49754b=JSON['parse']($response['body']);_0x49754b['license']['status']='sub';_0x49754b['license']['expireTime']=0x1b8d90e4000;$done({'body':JSON['stringify'](_0x49754b)});
